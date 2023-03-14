9eb55940f1eb67708e4fa303685668e5984887b7
Djoult <55453354+Djoult@users.noreply.github.com>
2023-03-14 21:06:18 +0000

    Deploying to gh-pages from @ Djoult/goit-js-hw-09@2f750404ff091383a332a162198db02949ae5cf6 🚀

--- 01-color-switcher.d0e14a80.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},n={start:function(){t.startBtn.disabled=!0,this.timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=t,console.log(t)}),1e3)},stop:function(){t.startBtn.disabled=!1,clearInterval(this.timerId)}};t.startBtn.addEventListener("click",n.start.bind(n)),t.stopBtn.addEventListener("click",n.stop.bind(n))}();
-//# sourceMappingURL=01-color-switcher.d0e14a80.js.map
