df8a7212415486b9c74cf8c6e734c5d50fea279b
Djoult <55453354+Djoult@users.noreply.github.com>
2023-03-14 23:02:55 +0200

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,15 +7,12 @@ const refs = {
   stopBtn: document.querySelector('button[data-stop]'),
 };
 
-const colorSwitcher = {
-  //   isActive: false,
-  start() {
-    // if (this.isActive) {
-    //   return;
-    // }
+refs.stopBtn.disabled = true;
 
-    // this.isActive = true;
+const colorSwitcher = {
+  start() {
     refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
     this.timerId = setInterval(() => {
       const currentColor = getRandomHexColor();
       document.body.style.background = currentColor;
@@ -23,8 +20,8 @@ const colorSwitcher = {
     }, 1000);
   },
   stop() {
-    // this.isActive = false;
     refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
     clearInterval(this.timerId);
   },
 };
